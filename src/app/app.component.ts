e4d7e60a769f7f65ca9be7fc700f3c1fbeea3f1b
Zyr00 <skidrow.jc@gmail.com>
2018-04-17 15:49:19 +0100

    added car parts to car parts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,36 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Ultra Racing';
-
-  carParts = [
-    {
-      id: 1,
-      name: 'Super Tires',
-      description: 'These are the very best',
-      inStock: 5,
-      price: 4.99
-    },
-    {
-      id: 2,
-      name: 'Reinforced Shocks',
-      description: 'Shocks made from kryptonite',
-      inStock: 4,
-      price: 9.99
-    },
-    {
-      id: 3,
-      name: 'Padded Seats',
-      description: 'Supper soft seats for a smooth ride',
-      inStock: 0,
-      price: 24.99
-    }
-  ]
-
-  totalCarParts(): number{
-    let num = 0;
-    for (let part of this.carParts) {
-      num += part.inStock; 
-    }
-    return num;
-  }
 }
--- src/app/car-parts/car-parts.component.ts
+++ src/app/car-parts/car-parts.component.ts
@@ -12,4 +12,35 @@ export class CarPartsComponent implements OnInit {
   ngOnInit() {
   }
 
+  carParts = [
+    {
+      id: 1,
+      name: 'Super Tires',
+      description: 'These are the very best',
+      inStock: 5,
+      price: 4.99
+    },
+    {
+      id: 2,
+      name: 'Reinforced Shocks',
+      description: 'Shocks made from kryptonite',
+      inStock: 4,
+      price: 9.99
+    },
+    {
+      id: 3,
+      name: 'Padded Seats',
+      description: 'Supper soft seats for a smooth ride',
+      inStock: 0,
+      price: 24.99
+    }
+  ]
+
+  totalCarParts(): number{
+    let num = 0;
+    for (let part of this.carParts) {
+      num += part.inStock; 
+    }
+    return num;
+  }
 }
